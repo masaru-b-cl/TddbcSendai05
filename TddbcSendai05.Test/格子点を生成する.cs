d7fcf9b425c4156e1726ab969c816b95f8fff4e8
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2015-11-16 15:52:43 +0900

    格子点を生成する

---
+++ TddbcSendai05.Test/格子点を生成する.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace TddbcSendai05.Test
+{
+  [TestFixture]
+  public class 格子点を生成する
+  {
+    [TestCase]
+    public void 整数を与えて生成した格子点のX座標を取得する()
+    {
+      GridPoint gp = new GridPoint(x: 1, y: 2);
+
+      gp.X.Is(1);
+    }
+
+    [TestCase]
+    public void 整数を与えて生成した格子点のY座標を取得する()
+    {
+      GridPoint gp = new GridPoint(x: 1, y: 2);
+
+      gp.Y.Is(2);
+    }
+
+    [TestCase]
+    public void 整数を与えて生成した格子点の文字列表記を取得する()
+    {
+      GridPoint gp = new GridPoint(x: 1, y: 2);
+
+      gp.ToString().Is("(1,2)");
+    }
+  }
+}
---
+++ TddbcSendai05/GridPoint.cs
@@ -0,0 +1,19 @@
+﻿namespace TddbcSendai05
+{
+  public class GridPoint
+  {
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public GridPoint(int x, int y)
+    {
+      this.X = x;
+      this.Y = y;
+    }
+
+    public override string ToString()
+    {
+      return $"({X},{Y})";
+    }
+  }
+}
