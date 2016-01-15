16ca7ea260cf430c65d215bd1d46ca261775fb85
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-01-15 12:58:10 +0900

    格子点集合が指定した格子点を含むか判定する

---
+++ TddbcSendai05.Test/格子点集合が指定した格子点を含むか判定する.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace TddbcSendai05.Test
+{
+  [TestFixture]
+  public class 格子点集合が指定した格子点を含むか判定する
+  {
+    [TestCase]
+    public void 空の格子点集合が指定した格子点を含まない()
+    {
+      var set = new GridPointSet(null, null);
+      bool contains = set.Contains(new GridPoint(0, 0));
+      contains.IsFalse();
+    }
+
+    [TestCase]
+    public void 格子点集合の1つ目の格子点と同じ格子点を含む()
+    {
+      var set = new GridPointSet(new GridPoint(0, 0), null);
+      bool contains = set.Contains(new GridPoint(0, 0));
+      contains.IsTrue();
+    }
+
+    [TestCase]
+    public void 格子点集合の2つ目の格子点と同じ格子点を含む()
+    {
+      var set = new GridPointSet(null, new GridPoint(0, 0));
+      bool contains = set.Contains(new GridPoint(0, 0));
+      contains.IsTrue();
+    }
+
+    [TestCase]
+    public void 格子点集合に同じ格子点を含まない()
+    {
+      var set = new GridPointSet(new GridPoint(0, 1), new GridPoint(1, 0));
+      bool contains = set.Contains(new GridPoint(0, 0));
+      contains.IsFalse();
+    }
+
+  }
+}
---
+++ TddbcSendai05/GridPointSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace TddbcSendai05
+{
+  public class GridPointSet
+  {
+    private GridPoint gridPoint1;
+    private GridPoint gridPoint2;
+
+    public GridPointSet(GridPoint gridPoint1, GridPoint gridPoint2)
+    {
+      this.gridPoint1 = gridPoint1;
+      this.gridPoint2 = gridPoint2;
+    }
+
+    public bool Contains(GridPoint target)
+    {
+      var gps = new[] { gridPoint1, gridPoint2 };
+      if (gps.Any(gp => gp != null && gp.HasSameCoordinatesWith(target)))
+      {
+        return true;
+      }
+      return false;
+    }
+  }
+}
